75066065a9835378ec3e619e21a5d9e7bb1cbdbd
Clint Herron <hanclinto@gmail.com>
2025-07-12 03:35:49 -0400

    Removing debug run from run_tests.sh

--- code/run_tests.sh
+++ code/run_tests.sh
@@ -1,3 +1,5 @@
 python -m pytest -rP tests/
 # NOTE: To run without output print spam, use:
 # python -m pytest tests/
+# Run only test_gx1_vaporator_full_tank
+# python -m pytest -rP tests/test_simulation_vaporators_simple.py::test_gx1_vaporator_full_tank
