ca4b3adb861358fd27d417e90900480bf3bec4f4
Ben Garney <ben.garney@gmail.com>
2025-08-09 14:04:08 -0700

    Rough draft

--- code/start_webgui.sh
+++ code/start_webgui.sh
@@ -1,1 +1,8 @@
-MF_DEFAULT_SCENARIO="$(dirname "$0")/example_scenario.json" uvicorn webgui.server:app --reload
+# If we are on Windows, use the full path to uvicorn
+# Otherwise, use the uvicorn command directly
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    UVICORN_CMD="/c/Python313/Scripts/uvicorn.exe"
+else
+    UVICORN_CMD="uvicorn"
+fi
+MF_DEFAULT_SCENARIO="$(dirname "$0")/example_scenario.json" $UVICORN_CMD webgui.server:app --reload
