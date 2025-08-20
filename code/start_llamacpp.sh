d26f3757086f160dbe938d8bcbccea5f0b846f2f
Clint Herron <hanclinto@gmail.com>
2025-08-19 21:49:16 -0400

    Making start_llamacpp.sh function cross-platform.

--- code/start_llamacpp.sh
+++ code/start_llamacpp.sh
@@ -1,1 +1,8 @@
-~/Downloads/llama-b5711-bin-win-cuda-12.4-x64/llama-server --jinja -fa -hf bartowski/Qwen2.5-7B-Instruct-GGUF:Q4_K_M
+# If we are on Windows, use the full path to llama-server
+# Otherwise, use the llama-server command directly
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    LLAMASERVER_CMD="~/Downloads/llama-b5711-bin-win-cuda-12.4-x64/llama-server"
+else
+    LLAMASERVER_CMD="llama-server"
+fi
+$LLAMASERVER_CMD --jinja -fa -hf bartowski/Qwen2.5-7B-Instruct-GGUF:Q4_K_M
